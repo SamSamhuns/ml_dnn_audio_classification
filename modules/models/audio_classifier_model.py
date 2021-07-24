7c9f388dc1ad94915f46d92a2c37a7f702a42699
SamSamhuns <samhunsadamant@gmail.com>
2021-07-24 19:35:37 +0400

    Add audio clsf model

---
+++ modules/models/audio_classifier_model.py
@@ -0,0 +1,60 @@
+"""
+An example for the model class
+"""
+from torch.nn import init
+import torch.nn as nn
+
+
+class AudioClassifier(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        conv_layers = []
+
+        # First Convolution Block with Relu and Batch Norm. Use Kaiming Initialization
+        self.conv1 = nn.Conv2d(2, 8, kernel_size=(
+            5, 5), stride=(2, 2), padding=(2, 2))
+        self.relu1 = nn.ReLU()
+        self.bn1 = nn.BatchNorm2d(8)
+        init.kaiming_normal_(self.conv1.weight, a=0.1)
+        self.conv1.bias.data.zero_()
+        conv_layers += [self.conv1, self.relu1, self.bn1]
+
+        # 2nd Convolution Block
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=(
+            3, 3), stride=(2, 2), padding=(1, 1))
+        self.relu2 = nn.ReLU()
+        self.bn2 = nn.BatchNorm2d(16)
+        init.kaiming_normal_(self.conv2.weight, a=0.1)
+        self.conv2.bias.data.zero_()
+        conv_layers += [self.conv2, self.relu2, self.bn2]
+        # 3rd Convolution Block
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=(
+            3, 3), stride=(2, 2), padding=(1, 1))
+        self.relu3 = nn.ReLU()
+        self.bn3 = nn.BatchNorm2d(32)
+        init.kaiming_normal_(self.conv3.weight, a=0.1)
+        self.conv3.bias.data.zero_()
+        conv_layers += [self.conv3, self.relu3, self.bn3]
+        # 4th Convolution Block
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=(
+            3, 3), stride=(2, 2), padding=(1, 1))
+        self.relu4 = nn.ReLU()
+        self.bn4 = nn.BatchNorm2d(64)
+        init.kaiming_normal_(self.conv4.weight, a=0.1)
+        self.conv4.bias.data.zero_()
+        conv_layers += [self.conv4, self.relu4, self.bn4]
+
+        # Linear Classifier
+        self.adap_pool = nn.AdaptiveAvgPool2d(output_size=1)
+        self.linear = nn.Linear(in_features=64, out_features=num_classes)
+
+        # Wrap the Convolutional Blocks
+        self.conv = nn.Sequential(*conv_layers)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.adap_pool(x)
+        x = x.view(x.shape[0], -1)
+
+        x = self.linear(x)
+        return x
